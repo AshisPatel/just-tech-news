8e8683308bf8a2d5a88b9d1c109c7aea61d08ecf
AP <ashis.n.patel@gmail.com>
2021-09-11 22:17:39 -0500

    Add get single post, update post title, create post, and delete post requests

--- models/index.js
+++ models/index.js
@@ -1,4 +1,13 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// Create associations - States that one user can have many posts
+User.hasMany(Post, {
+    foreignkey: 'user_id'
+});
+// This association states that a post can belong to one user, not many users 
+Post.belongsTo(User, {
+    foreignKey:'user_id'
+});
+
 module.exports = { User, Post };
